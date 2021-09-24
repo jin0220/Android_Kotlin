894eca561b50636ff3287f14b7b3d8d5bc452131
jin0220 <37447282+jin0220@users.noreply.github.com>
2021-09-24 19:36:52 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn1.setOnClickListener {
+            Toast.makeText(this, "click!",Toast.LENGTH_SHORT).show()
+        }
+
+        btn2.setOnClickListener {
+
+        }
+    }
+}
